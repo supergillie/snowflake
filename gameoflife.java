754119c19ce3e49efbc5df988c4f59d2f9cf504e
supergillie <peter.krooks@gmail.com>
2020-07-01 09:55:49 +0200

    14

---
+++ gameoflife.java
@@ -0,0 +1,6 @@
+public class gameoflife{
+    public static void main (String[] args)
+    {
+        System.out.println("hejsan hopphjjhsan svjhjhejsan");
+    }
+}
--- src/gameoflife_old.java
+++ src/gameoflife_old.java
@@ -1,4 +1,4 @@
-class gameoflife {
+class gameoflife_old {
     public static void main (String[] args)
     {
         System.out.println("hejsan hoppsan svjhjhejsan");
