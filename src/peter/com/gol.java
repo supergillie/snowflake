ff7bc19679a8b60d4125ae0a5033fa20fd891f39
supergillie <peter.krooks@gmail.com>
2020-07-01 09:09:09 +0200

    rättar felet

--- src/peter/com/gol.java
+++ src/peter/com/gol.java
@@ -3,6 +3,6 @@ package peter.com;
 class gol{
 
 public static void main (String[] args){
-    System.ou t.println("hejsan hoppsan svjhjhejsan");
+    System.out.println("hejsan hoppsan svjhjhejsan");
 }
 }
