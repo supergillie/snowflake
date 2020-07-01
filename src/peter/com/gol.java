d049a0447781349580994fef5ee46d4b96b09583
supergillie <peter.krooks@gmail.com>
2020-07-01 09:07:13 +0200

    lägger till fel för att testa

--- src/peter/com/gol.java
+++ src/peter/com/gol.java
@@ -3,6 +3,6 @@ package peter.com;
 class gol{
 
 public static void main (String[] args){
-    System.out.println("hejsan hoppsan svjhjhejsan");
+    System.ou t.println("hejsan hoppsan svjhjhejsan");
 }
 }
