d567038a63a2e584c72eb3eadb9fc38edd64eccd
supergillie <peter.krooks@gmail.com>
2020-07-01 09:24:20 +0200

    test 3

--- src/peter/com/gol.java
+++ src/peter/com/gol.java
@@ -1,8 +1,8 @@
 package peter.com;
 
 class gol{
-
-public static void main (String[] args){
-    System.out.println("hejsan hoppsan svjhjhejsan");
-}
+    public static void main (String[] args)
+    {
+        System.out.println("hejsan hoppsan svjhjhejsan");
+    }
 }
